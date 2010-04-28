e33d0388f7f9e50cdabd92b939fd99413579cbec
Jonty <jonty@jonty.co.uk>
2010-04-29 00:33:50 +0800

    Restore microdoc

--- examples/command_runner.py
+++ examples/command_runner.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 import os, sys, re, subprocess
 
+# If this script is set as your command handler, when any ?command is run in IRC it will
+# look in the path defined below for a script matching that command name and run it.
+#
+# e.g. ?uptime would look in "/usr/share/irccat/" (the default) for any script called
+# "uptime", with any extension. It would happily run both uptime.sh and uptime.py, or
+# a script in whatever language you like. Command names are limited to [0-9a-z].
+
 path = '/usr/share/irccat/'
 
 args = sys.argv[1]
