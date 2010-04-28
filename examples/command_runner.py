5d862e4982a4a67b01c5b1386d6213d8a15c19dc
Jonty <jonty@jonty.co.uk>
2010-04-29 00:32:17 +0800

    Fix argument passthrough weirdness, actually split bits out into real args

--- examples/command_runner.py
+++ examples/command_runner.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
 import os, sys, re, subprocess
 
-# If this script is set as your command handler, when any ?command is run in IRC it will
-# look in the path defined below for a script matching that command name and run it.
-#
-# e.g. ?uptime would look in "/usr/share/irccat/" (the default) for any script called
-# "uptime", with any extension. It would happily run both uptime.sh and uptime.py, or
-# a script in whatever language you like. Command names are limited to [0-9a-z].
-
 path = '/usr/share/irccat/'
 
 args = sys.argv[1]
@@ -20,7 +13,10 @@ if re.match('^[a-z0-9]+$', command):
 
         if re.match('^%s\.[a-z]+$' % command, file):
             found = True
-            proc = subprocess.Popen([path+file, args], shell=True, stdout=subprocess.PIPE)
+            
+            procArgs = [path + file]
+            procArgs.extend(bits)
+            proc = subprocess.Popen(procArgs, stdout=subprocess.PIPE)
             stdout = proc.stdout
 
             while True:
